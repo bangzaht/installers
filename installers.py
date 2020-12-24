86d684842b4bdf5689c18f25d4e5beea29a26c4b
xclowns <75274683+bangzaht@users.noreply.github.com>
2020-12-24 11:19:07 +0700

    Add files via upload

---
+++ installers.py
@@ -0,0 +1,260 @@
+#!/bin/bash
+#version 1.0
+
+#                       # AutHoR:BaNG zAhtT
+#                       YG KESINI UNTUK BELAJAR SELAMAT^_^  ,BELAJAR JANGN DIRECOD YAK:)
+#                       THANKS DH PAKE SC INI^_^
+
+#20/03/19
+
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+echo
+echo
+echo
+echo  "                 _________________________" | lolcat
+echo  "                 |                       |" | lolcat
+echo  "                 | GUNAKAN DENGAN BIJAK    |" | lolcat
+echo  "###########################################################" | lolcat
+echo  "                  TOOLS INSTALLER  TERMUX " |lolcat
+echo  ""
+echo  "############################################################" | lolcat
+echo  "#  AUTHOR     : BaNG ZaHtT   " | lolcat
+echo  "#----------------------------------------------------------#" | lolcat
+echo  "#  FaceBook   : 404USER   " | lolcat
+echo  "#----------------------------------------------------------#" | lolcat
+echo  "#  WhatsApp   : +6283874438104  "  |lolcat
+echo  "#----------------------------------------------------------#" | lolcat
+echo  "###########################################################" | lolcat
+
+###################################################                                             
+# CTRL + C                                        #
+###################################################
+
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $green"[#]> (Ctrl + C ) Detected, Trying To Exit ... " |lolcat
+echo -b $green"[#]> THANKS UDAH PAKE TOOLS GUE " |lolcat
+sleep 1
+echo ""
+echo -b $green"[#]>  COMEBACK AGAIN BROTHER :)" |lolcat
+
+echo -b $green"[#]> RooT./T-REX WASS HERE !" |lolcat
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo ""
+echo ""
+
+echo  "####################################" | lolcat
+echo  "#SILAHKAN PAKAI TOOLS YANG TERSEDIA#" | lolcat
+echo  "####################################" | lolcat
+                                                                                                
+echo ""
+echo  "WELLCOME TO MY TOOLS COEG^_^" |lolcat
+echo  "JANGAN DISALAH GUNAKAN ANJG!" | lolcat
+echo  "BELAJAR SENDIRI MEMANGLAH SUSAH,TAPI RASA PUAS YANG DIDAPAT BEGITU MELIMPAH:)" | lolcat
+echo  "HARGAI AUTHOR TOD!" | lolcat
+echo  "KEGAGALAN ADALAH AWAL DARI KESUKSESAN " | lolcat
+echo  "SELAMAT BELAJAR!:)" | lolcat
+echo  "RooT./BaNG ZahtT" | lolcat
+echo  "......ENJOYY BRAY^_^....." | lolcat
+echo ""
+echo  "TUNGGU PROSESNYA............" | lolcat
+echo ""
+echo ""
+
+
+echo ""
+echo -e $b "1. HACK CCTV INDOMART${enda}";
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "2. LAZYMUX${enda}";
+echo -e "<[¤]>=========================<[¤]>" | lolcat                          
+echo -e $b "3. DEFACE${enda}";
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "4. SCRIPT-DEFACE-CREATOR${enda}";
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "5. WEEMAN${enda}";
+echo -e "<[¤]>=========================<[¤]>" | lolcat 
+echo -e $b "6. HYDRA${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "7. IPCS${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "8. OSIF ${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "9. MBF${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "10. CLONE YAHOO${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "11. Hack FB${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "12. MALICIOUS${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "13. TOOLS BLACK EYE${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "14. KO-DORK${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "15. HACK WIFI(ROOT)${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "00. Exit${enda}";
+echo -e "╭─RooT./BANgZAhT [PILIH AJA NOMERNYA]" | lolcat
+read -p "╰──────#>>>" pil;
+
+
+case  $pil in
+1) figlet -f slant MR.T-REX...
+echo  "Script rusak:(" | lolcat
+echo  "Sory coeg" | lolcat
+echo "Mohon Maaf Atas Ketidak nyamanannya" | lolcat
+echo "Pake yg laen aja yak^_^" | lolcat
+
+;;
+
+2) figlet -f slant MR.T-REX. ...
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+
+
+;;
+
+3) figlet -f slant MR.T-REX...
+apt update && apt upgrade -y
+apt install git
+git clone https://github.com/Amriez/AOCDEFACE
+cd AOCDEFACE
+
+;;
+
+4) figlet -f slant MR.T-REX...
+apt update
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/Ubaii/script-deface-creator
+
+;;
+
+5) figlet -f slant MR.T-REX...
+apt update && apt upgrade
+apt install python2 git -y
+git clone https://github.com/evait-security/weeman.git
+cd weeman
+
+;;
+
+6) figlet -f slant MR.T-REX...
+ apt update && apt install -y wget
+apt install hydra
+wget 
+http://scrapmaker.com/download/data/wordlists/dictionaries/rockyou.txt
+
+;;
+
+7) figlet -f slant MR.T-REX...
+apt update && apt upgrade
+pkg install python2
+pkg install git
+pip2 install requests
+git clone https://github.com/kancotdiq/ipcs
+
+;;
+
+8) figlet -f slant MR.T-REX...
+lapt update && apt upgrade
+apt install python2
+pip2 install requests mechanize
+apt install git
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+
+
+;;
+
+9) figlet -f slant MR.T-REX...
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/hnov7/mbf
+cd mbf
+
+;;
+
+10) figlet -f slant MR.T-REX...
+apt update && apt upgrade
+git clone https://github.com/FR13ND8/EmailVuln
+
+;;
+
+11) figlet -f slant MR.T-REX...
+apt update && apt upgrade
+apt install python2
+git clone https://github.com/MRT-REX/Crack-Facebook
+
+;;
+
+12) figlet -f slant MR.T-REX...
+apt update && apt upgrade
+apt install git python2 -y
+git clone https://github.com/Hider5/Malicious
+
+
+;;
+
+13) figlet -f slant MR.T-REX...
+pkg install bash
+pkg install git
+git clone https://github.com/thelinuxchoice/blackeye
+
+
+;;
+
+14) figlet -f slant MR.T-REX...
+pkg update upgrade
+pkg install git python2
+git clone https://github.com/ciku370/ko-dork
+cd ko-dork
+
+;;
+
+
+15) figlet -f slant MR.T-REX...
+pkg update
+pkg install git
+git clone https://github.com/MRT-REX/Wifi-Hack
+mv WiFiHack.apk /sdcard
+
+;;
+
+
+00 )echo "BYE COEGG...JAN LUPA KASIH STAR YAK^_^" | lolcat
+exit
+
+;;
+
+*) echo "Okeh..Pilih nomernya aja bngsd_-" | lolcat
+
+esac
+done
+done
